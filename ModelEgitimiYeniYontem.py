4caf486d99ed3fda50f231f69c7878d61a784a89
SonDilBukuculer <87260661+SonDilBukuculer@users.noreply.github.com>
2021-08-26 22:08:27 +0300

    Kendi veri kümeniz ile modelinizi eğitebilirsiniz.

--- ModelEgitimiYeniYontem.py
+++ ModelEgitimiYeniYontem.py
@@ -28,7 +28,7 @@ warnings.filterwarnings('ignore')
     # torch.cuda.device_count()
     # torch.cuda.get_device_name(0)
 
-checkpoint = "sentence-transformers/paraphrase-MiniLM-L12-v2"
+checkpoint = "sentence-transformers/paraphrase-MiniLM-L3-v2"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
 
